3ef86d8957df90ca05f3a40f5747d111153c1f70
BrunoGomes10 <brunogomes.gomes@gmail.com>
2023-01-02 16:42:20 -0400

    AULA#02 Finish

--- App.tsx
+++ App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import AppLoading from 'expo-app-loading';
 
-import { Welcome } from './src/pages/Welcome';
-import { useFonts, Jost_400Regular, Jost_600SemiBold } from '@expo-google-fonts/jost'
+import Routes from './src/routes';
+
+import { 
+  useFonts, 
+  Jost_400Regular, 
+  Jost_600SemiBold 
+} from '@expo-google-fonts/jost'
 
 export default function App(){
   const [fontsLoaded]  = useFonts({
@@ -15,7 +20,7 @@ export default function App(){
   }
 
   return(
-    <Welcome />
+    <Routes />
   )
 }
 
