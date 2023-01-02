2fa37d0287afe331669c1f7bdc526e1d9c401fe5
BrunoGomes10 <brunogomes.gomes@gmail.com>
2023-01-02 16:38:02 -0400

    AULA#02 FINALIZADA

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,41 +1,72 @@
 import React from 'react';
-import { SafeAreaView, Text, Image, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
+import { SafeAreaView, 
+        Text, 
+        Image, 
+        TouchableOpacity, 
+        StyleSheet, 
+        Dimensions,
+        View } from 'react-native';
 
 import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/core';
 
 import wateringImg from '../assets/watering.png';
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
+
 export function Welcome(){
+    const navigation = useNavigation();
+
+    function handleStart(){
+        navigation.navigate('UserIdentification')
+    }
 
     return(
         <SafeAreaView style={styles.container}>
-            <Text style={styles.title}>
-                Gerencie {'\n'}
-                suas plantas de {'\n'}
-                forma fácil
-            </Text>
+            <View style={styles.wrapper}>
+                <Text style={styles.title}>
+                    Gerencie {'\n'}
+                    suas plantas de {'\n'}
+                    forma fácil
+                </Text>
 
-            <Image source={wateringImg} style={styles.image} resizeMode="contain"/>         
+                <Image 
+                    source={wateringImg} 
+                    style={styles.image} 
+                    resizeMode="contain"
+                />         
 
-            <Text style={styles.subtitle}>
-                Não esqueça mais de regar as suas plantas.
-                Nós cuidamos de lembrar você sempre que precisar.
-            </Text>
+                <Text style={styles.subtitle}>
+                    Não esqueça mais de regar as suas plantas.
+                    Nós cuidamos de lembrar você sempre que precisar.
+                </Text>
 
-            <TouchableOpacity style={styles.button} activeOpacity= {0.7} >
-                <Feather name="chevron-right" style={styles.buttonIcon}                    />
-            </TouchableOpacity>
+                <TouchableOpacity 
+                    style={styles.button} 
+                    activeOpacity= {0.7} 
+                    onPress={handleStart}
+                >
+                    <Feather 
+                        name="chevron-right" 
+                        style={styles.buttonIcon} />
+                </TouchableOpacity>
+            </View>
+
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1
+    },
+
+    wrapper: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'space-around'
+        justifyContent: 'space-around',
+        paddingHorizontal: 20
     },
 
     title: {
@@ -49,10 +80,10 @@ const styles = StyleSheet.create({
     },
 
     subtitle: {
-        fontSize: 18,
         textAlign: 'center',
-        color: colors.heading,
+        fontSize: 18,        
         paddingHorizontal: 20,
+        color: colors.heading,        
         fontFamily: fonts.text
     },
 
